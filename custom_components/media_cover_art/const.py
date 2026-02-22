f8e550ff739465655985bbff784c965beaf6d71d
Benjamin <95508027+Levtos@users.noreply.github.com>
2026-02-23 00:39:47 +0100

    Make coordinator fail-safe and expose diagnostics in status sensor

--- custom_components/media_cover_art/const.py
+++ custom_components/media_cover_art/const.py
@@ -4,13 +4,17 @@ from homeassistant.const import Platform
 
 DOMAIN = "media_cover_art"
 
-PLATFORMS: list[Platform] = [Platform.IMAGE]
+PLATFORMS: list[Platform] = [Platform.IMAGE, Platform.SENSOR]
 
 CONF_SOURCE_ENTITY_ID = "source_entity_id"
 CONF_PROVIDERS = "providers"
 CONF_ARTWORK_SIZE = "artwork_size"
+CONF_ARTWORK_WIDTH = "artwork_width"
+CONF_ARTWORK_HEIGHT = "artwork_height"
 
 PROVIDER_ITUNES = "itunes"
 
 DEFAULT_PROVIDERS: list[str] = [PROVIDER_ITUNES]
 DEFAULT_ARTWORK_SIZE = 600
+DEFAULT_ARTWORK_WIDTH = 600
+DEFAULT_ARTWORK_HEIGHT = 600
